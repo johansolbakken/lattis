a2f2605bd39eef9fcec0df4e968aa7f463d194e7
Johan Solbakken <johsol@stud.ntnu.no>
2023-04-17 19:54:39 +0200

    reaching definitions main

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ use parser::Parser;
 mod lexer;
 mod node;
 mod parser;
+mod analysis;
 
-fn main() {
+fn generate_graph() -> parser::Node {
     let text = std::fs::read_to_string("oppg.txt").unwrap();
     let mut lexer = Lexer::new(text);
     let tokens = lexer.lex_all();
     let mut parser = Parser::new(tokens);
     let mut root = parser.parse();
-    let root2 = node::simplify(&mut root);
-    let s = root2.to_string(0);
-    println!("{}", s);
+    node::simplify(&mut root)
+}
+
+fn main() {
+    let root = generate_graph();
+    let iterations = analysis::reaching_definitions(&root);
 }
